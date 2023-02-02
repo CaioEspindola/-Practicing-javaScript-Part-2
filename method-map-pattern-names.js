573224c84d0eaa28afa840ab2dbb1df141072fb0
Caio Espindola <caioespindola@hotmail.com>
2023-02-02 10:24:24 -0300

    method map, names pattern

---
+++ method-map-pattern-names.js
@@ -0,0 +1,7 @@
+//Name pattern. Letting all names with uppercase.
+
+let heroesNames = ['wolverine', 'iron Man', 'Thor', 'spider Man', 'hulk']
+
+const patternNames = heroesNames.map(name => name.toUpperCase())
+
+console.log(patternNames)
